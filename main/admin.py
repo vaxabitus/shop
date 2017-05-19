767b868a94143c537a41843b3983f0b4e0a33761
Denis Lugovoi <Denis_Lugovoi@epam.com>
2017-05-19 16:29:44 +0400

    Add models and admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Item
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+# from datetime import *
+# from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название категории")
+    alias = models.SlugField(verbose_name="Alias категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return 'Категория %s' % self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название товара")
+    price = models.IntegerField(default=0, verbose_name="Цена")
+    image = models.CharField(max_length=255, verbose_name="Ссылка на картинку")
+    alias = models.SlugField(verbose_name="Alias товара")
+
+    category = models.ForeignKey(Category)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return 'Товар %s' % self.name
