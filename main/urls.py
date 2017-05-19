a798213cbdc3cd23515e203886a35c8a0fa352fa
Denis Lugovoi <Denis_Lugovoi@epam.com>
2017-05-19 18:51:21 +0400

    first Detail page

--- main/urls.py
+++ main/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^item/(?P<alias>[^/]+)', views.item, name="item"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
-from .models import *
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+# from django.core.exceptions import ObjectDoesNotExist
 from django.template.loader import render_to_string
-from django.template.context_processors import csrf
-from django.views.decorators.csrf import csrf_exempt
-from datetime import *
-import random
-import string
+# from django.template.context_processors import csrf
+# from django.views.decorators.csrf import csrf_exempt
+# from datetime import *
+# import random
+# import string
+from .models import Item, Category
 # Create your views here.
 
 
 def home(request):
-    tovars = []
-    for x in range(0, 3):
-        tovars.append(x)
+    tovars = Item.objects.all()
     context = {
         'title': 'HelloWorld',
         'tovars': tovars,
     }
-    return HttpResponse(render_to_string('index.html', context))
+    return render(request, 'index.html', context)
+
+
+def item(request, alias):
+    try:
+        tovar = Item.objects.get(alias=alias)
+    except:
+        return Http404
+    context = {
+        'tovar': tovar,
+    }
+    return HttpResponse(render_to_string('item.html', context))
